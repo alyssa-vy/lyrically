94c80368faf13d384adec74625315408eae83655
michaeltothewilson <56896666+michaeltothewilson@users.noreply.github.com>
2020-11-10 11:28:37 -0800

    made a server folder

---
+++ server/server.js
@@ -0,0 +1,9 @@
+// ***** Lyrically server.js *****
+
+// Initialize Genius Api
+import Genius from 'genius-api'
+
+const token = 'iUGxM6KvpAfW6FQbnAwjRbajgerQ94bT4kErIUF-GjlGnGeiRkh33qukex-5AUmL'
+const genius = new Genius(token)
+
+
