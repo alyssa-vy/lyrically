42d150622a3a28729854535c56b6f6d811a2f028
knzgage <40836138+knzgage@users.noreply.github.com>
2020-11-21 21:29:49 -0800

    Cleaned up search functions

--- javascript/generate-results.js
+++ javascript/generate-results.js
@@ -1,30 +1,8 @@
-let results = [];
-let result1 = {artist: "Artist1", title: "Title1", album: "Album1", artwork: "http://placekitten.com/200/200"}
-let result2 = {artist: "Artist2", title: "Title2", album: "Album2", artwork: "http://placekitten.com/200/200"}
-let result3 = {artist: "Artist3", title: "Title3", album: "Album3", artwork: "http://placekitten.com/200/200"}
-let result4 = {artist: "Artist4", title: "Title4", album: "Album4", artwork: "http://placekitten.com/200/200"}
-let result5 = {artist: "Artist5", title: "Title5", album: "Album5", artwork: "http://placekitten.com/200/200"}
-let result6 = {artist: "Artist6", title: "Title6", album: "Album6", artwork: "http://placekitten.com/200/200"}
-let result7 = {artist: "Artist7", title: "Title7", album: "Album7", artwork: "http://placekitten.com/200/200"}
-let result8 = {artist: "Artist8", title: "Title8", album: "Album8", artwork: "http://placekitten.com/200/200"}
-let result9 = {artist: "Artist9", title: "Title9", album: "Album9", artwork: "http://placekitten.com/200/200"}
-let result10 = {artist: "Artist10", title: "Title10", album: "Album10", artwork: "http://placekitten.com/200/200"}
-results.push(result1); 
-results.push(result2); 
-results.push(result3);
-results.push(result4); 
-results.push(result5); 
-results.push(result6); 
-results.push(result7); 
-results.push(result8); 
-results.push(result9); 
-results.push(result10); 
-
-let container = document.getElementById("results-container");
-
+// Creates a card based off of song information
 function createCard(song){
     let card = document.createElement("div");
     card.className += "song-card";
+    card.id = song.id;
 
     let cardArtist = document.createElement("div");
     let cardTitle = document.createElement("div");
@@ -52,7 +30,12 @@ function createCard(song){
     return card;
 }
 
-for (song of results) {
-    let songCard = createCard(song);
-    container.appendChild(songCard);
+
+//Takes all results and makes them into cards before adding them to the results container
+function addCardsToPage(results) {
+    let container = document.getElementById("results-container");
+    for (song of results) {
+        let songCard = createCard(song);
+        container.appendChild(songCard);
+    }
 }
--- javascript/search.js
+++ javascript/search.js
@@ -1,27 +1,41 @@
 //Takes a lyric from the user and retrieves data of song related to that lyric
-
 function performSearch(lyric){
+    // window.location.href = "../html/results.html";
+    songResults = convertResults(lyric);
+    songResults.then( (result) => {
+        console.log(result);
+    })
+}
+
+
+//Takes a lyric from the user and retrieves data of song related to that lyric
+function convertResults(lyric) {
     let songResults = [];
-    const apiResults = getSong(lyric);
 
     // Take the api's response for a search and extract the data needed
-    apiResults.then(data =>{
-        console.log(data);
+    return new Promise((resolve, reject) => {
 
-        // Put required data into a song object
-        for (result of data){
-            // let artResults = getArt(result.track.album_id);
-            // artResults.then(art => {
-            //     console.log(art);
-            // })
-            let song = {
-                artist: result.track.artist_name,
-                title: result.track.track_name,
-                album: result.track.album_name
-                // artwork: getArt(result.track.albumid)
-            };
-            songResults.push(song);
-        }
-        console.log(songResults);
+        const apiResults = getSong(lyric);
+        apiResults.then(data =>{
+
+            // Put required data into a song object
+            for (result of data){
+                // let artResults = getArt(result.track.album_id);
+                // artResults.then(art => {
+                //     console.log(art);
+                // })
+                let song = {
+                    artist: result.track.artist_name,
+                    title: result.track.track_name,
+                    album: result.track.album_name,
+                    id: result.track.track_id,
+                    artwork: "http://placekitten.com/200/200" //temp holder
+                    // artwork: getArt(result.track.albumid)
+                };
+                // add the song to the results
+                songResults.push(song);
+            }
+            resolve(songResults);
+        })
     })
 }
