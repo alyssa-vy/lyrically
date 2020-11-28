f31ac7459fa9e58de3d354c48a93c260cb60bb00
michaeltothewilson <56896666+michaeltothewilson@users.noreply.github.com>
2020-11-28 10:22:05 -0800

    process.env.PORT added for heroku

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ spotifyApi.clientCredentialsGrant().then(
 
 // Const Starter Pack
 const hostname = 'localhost'
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
