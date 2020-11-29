046c2519740615a9f099886787db64e091651a7f
knzgage <40836138+knzgage@users.noreply.github.com>
2020-11-28 16:15:44 -0800

    Fixed Results Accessability Errors
    Added alt text for album art, and added a label for the search form element

--- public/javascript/generate-results.js
+++ public/javascript/generate-results.js
@@ -232,7 +232,8 @@ function createCard(song, songNum){
     let artistInfo = document.createTextNode(`Artist: ${song.artist}`);
     let titleInfo = document.createTextNode(`Song: ${song.title}`);
     let albumInfo = document.createTextNode(`Album: ${song.album}`);
-    // cardArtwork.src = song.artwork;
+
+    cardArtwork.alt = `${song.title} Cover`;
 
     // Fetch and set the artwork
     let artResults = getSpotifyAlbumArt(song.artist);
